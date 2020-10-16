269b242129d5786e62ad1e072d697d14b634dfd9
Oleg <oleg_kolpakov@icloud.com>
2020-10-16 17:03:49 +0500

    Second Practice with decriment for cycle (strange for cycle)

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import "./App.css";
 
 function App() {
   const numberOfFilms = +prompt("How many films did you watch?", "");
-  const lastSeeingFilm = prompt("Last seeing film?", "");
-  const lastFilmRating = +prompt("Rate it!", "");
   const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -12,8 +10,38 @@ function App() {
     genres: [],
     privat: false,
   };
+  // if (numberOfFilms) {
+  //   numberOfFilms = +prompt("How many films did you watch?", "");
+  // }
 
-  personalMovieDB.movies[lastSeeingFilm] = lastFilmRating;
+  for (let i = 0; i < 2; i++) {
+    const lastSeeingFilm = prompt("Last seeing film?", "");
+    const lastFilmRating = +prompt("Rate it!", "");
+    if (
+      lastSeeingFilm != null &&
+      lastFilmRating != null &&
+      lastSeeingFilm != "" &&
+      lastFilmRating != "" &&
+      lastSeeingFilm.length < 50
+    ) {
+      console.log("done");
+    } else {
+      console.log("error");
+      i--;
+    }
+    // personalMovieDB.movies[lastSeeingFilm] = lastFilmRating;
+  }
+
+  if (personalMovieDB.count < 10) {
+    console.log("Too little");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Classic watcher");
+  } else if (personalMovieDB.count >= 30) {
+    console.log("Cinema lover!");
+  } else {
+    console.log("error");
+  }
+
   console.log(personalMovieDB);
   return (
     <div className="App-header">
