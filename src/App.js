df7feb33a53f225f6476857e226c42e0389c711a
Oleg Kolpakov <oleg_kolpakov@icloud.com>
2020-11-25 14:08:30 +0500

    Back to practice and third task (adding some functions for working with Database)

--- src/App.js
+++ src/App.js
@@ -2,47 +2,88 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  const numberOfFilms = +prompt("How many films did you watch?", "");
+  let numberOfFilms;
+
+  function start() {
+    numberOfFilms = +prompt("How many films did you watch?", "");
+
+    while (
+      numberOfFilms == "" ||
+      numberOfFilms == null ||
+      isNaN(numberOfFilms)
+    ) {
+      numberOfFilms = +prompt("How many films did you watch?", "");
+    }
+  }
+
+  start();
+
   const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: false,
+    private: false,
   };
   // if (numberOfFilms) {
   //   numberOfFilms = +prompt("How many films did you watch?", "");
   // }
 
-  for (let i = 0; i < 2; i++) {
-    const lastSeeingFilm = prompt("Last seeing film?", "");
-    const lastFilmRating = +prompt("Rate it!", "");
-    if (
-      lastSeeingFilm != null &&
-      lastFilmRating != null &&
-      lastSeeingFilm != "" &&
-      lastFilmRating != "" &&
-      lastSeeingFilm.length < 50
-    ) {
-      console.log("done");
+  function rememberMyFilms() {
+    for (let i = 0; i < numberOfFilms; i++) {
+      const lastSeeingFilm = prompt("Last seeing film?", "");
+      const lastFilmRating = +prompt("Rate it!", "");
+      if (
+        lastSeeingFilm !== null &&
+        lastFilmRating !== null &&
+        lastSeeingFilm !== "" &&
+        lastFilmRating !== "" &&
+        lastSeeingFilm.length < 50
+      ) {
+        console.log("done");
+      } else {
+        console.log("error");
+        i--;
+      }
+      // personalMovieDB.movies[lastSeeingFilm] = lastFilmRating;
+    }
+  }
+
+  // rememberMyFilms();
+
+  function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+      console.log("Too little");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+      console.log("Classic watcher");
+    } else if (personalMovieDB.count >= 30) {
+      console.log("Cinema lover!");
     } else {
       console.log("error");
-      i--;
     }
-    // personalMovieDB.movies[lastSeeingFilm] = lastFilmRating;
   }
 
-  if (personalMovieDB.count < 10) {
-    console.log("Too little");
-  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Classic watcher");
-  } else if (personalMovieDB.count >= 30) {
-    console.log("Cinema lover!");
-  } else {
-    console.log("error");
+  // detectPersonalLevel();
+
+  // console.log(personalMovieDB);
+
+  function showMyDB(hidden) {
+    if (!hidden) {
+      console.log(personalMovieDB);
+    }
   }
 
-  console.log(personalMovieDB);
+  showMyDB(personalMovieDB.private);
+
+  function writeYourGenres() {
+    for (let i = 1; i <= numberOfFilms; i++) {
+      personalMovieDB.genres[i - 1] = prompt(
+        `Your favorite genre in count of ${i}`
+      );
+    }
+  }
+  writeYourGenres();
+
   return (
     <div className="App-header">
       <h1> All watched - {personalMovieDB.count}</h1>
